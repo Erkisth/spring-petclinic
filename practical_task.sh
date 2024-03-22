bfc5398ad3f9304f7e9c6b6a12389068c05f490f
Erkisth <106119905+Erkisth@users.noreply.github.com>
2024-03-22 14:39:20 +0100

    Update practical_task.sh

--- practical_task.sh
+++ practical_task.sh
@@ -10,12 +10,12 @@ gcloud compute networks create script-network \
 gcloud compute networks subnets create script-subnet-1 \
     --network=script-network \
     --range=10.0.1.0/24 \
-    --region=us-central1-b
+    --region=us-central1
 
 gcloud compute networks subnets create script-subnet-2 \
     --network=script-network \
     --range=10.0.2.0/24 \
-    --region=us-central1-c
+    --region=us-central1
 
 gcloud artifacts repositories create script-repo \
     --repository-format=docker \
@@ -25,9 +25,16 @@ docker tag spring-petclinic us-central1-docker.pkg.dev/${PROJECT_NAME}/script-re
 
 docker push us-central1-docker.pkg.dev/${PROJECT_NAME}/script-repo/spring-petclinic:latest
 
-gcloud compute instances creat-with-container script-instance \
+gcloud compute instances create-with-container script-instance \
     --machine-type=e2-medium \
-    --zone=us-central1-b \
+    --zone=us-central1-a
     --network=script-network \
     --subnet=script-subnet-1 \
-    --container-image=us-central1-docker.pkg.dev/${PROJECT_NAME}/script-repo/spring-petclinic:latest
+    --container-image=us-central1-docker.pkg.dev/${PROJECT_NAME}/script-repo/spring-petclinic:latest \
+    --tags=http-server
+
+gcloud compute firewall-rules create allow-http \
+    --allow tcp:80 --target-tags http-server
+
+gcloud compute firewall-rules create allow-ssh \
+    --allow tcp:22 --target-tags http-server
